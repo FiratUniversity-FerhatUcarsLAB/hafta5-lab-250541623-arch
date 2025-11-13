91f76e251e2724f37d3fa83bcf36566dedad25b2
250541623-arch <250541623@firat.edu.tr>
2025-11-13 10:15:53 +0300

    FizikFormul.java

--- FizikFormul.java
+++ FizikFormul.java
@@ -1,7 +1,7 @@
 /*
- * Ad Soyad: [ADINIZI BURAYA YAZIN]
- * Ogrenci No: [OGRENCI NUMARANIZI BURAYA YAZIN]
- * Tarih: [TARIHI BURAYA YAZIN]
+ * Ad Soyad: [mustafa haccar]
+ * Ogrenci No: [250541623]
+ * Tarih: [13/11/2025]
  * Aciklama: Gorev 2 - Fizik Formulu Asistani
  *
  * Bu program temel fizik formullerini kullanarak
